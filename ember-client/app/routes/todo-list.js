42c38a8812c81dd08707fddae314272ad7ae2a7f
guruyaya@gmail.com <guruyaya@gmail.com>
2021-05-30 11:47:29 +0300

    Loading todo items

--- ember-client/app/routes/todo-list.js
+++ ember-client/app/routes/todo-list.js
@@ -2,15 +2,19 @@ import Route from '@ember/routing/route';
 import config from '../config/environment';
 
 export default Route.extend({
-    model: function() {
-        $.ajax({
-            'url': config.laravel_server + '/is_logged_in',
+    setupController: function(controller, mode) {
+        var todo_items = controller.get('todo_items').setObjects([]);
+        Ember.$.ajax({
+            url: config.laravel_server + '/todo-list',
             crossDomain: true,
             xhrFields: { withCredentials: true }
         }).then(function(data) {
-            if (data.logged_in == false) {
-                window.location.href = '/loggedout'
+            if (data.success == false) {
+                window.location.href = '/loggedout';
             }
+            $.each(data.todo_items, function() {
+                todo_items.pushObject(this);
+            });
         });
     }
 });
