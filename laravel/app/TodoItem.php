427748df3b7ad4b882fbe82deff4e3fb7adfbeee
guruyaya@gmail.com <guruyaya@gmail.com>
2021-06-03 20:11:20 +0300

    BUGGY: Can't mark done tasks, if not mine, will handle shortly

--- laravel/app/TodoItem.php
+++ laravel/app/TodoItem.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\SharedItem;
+
 use Illuminate\Database\Eloquent\Model;
 
 class TodoItem extends Model
@@ -12,12 +14,14 @@ class TodoItem extends Model
     }
     public static function get_user_tasks(int $user_id) {
         return self::leftJoin('shared_items', 'todo_items.id', '=', 'shared_items.todo_item_id')->
-            groupBy(['todo_items.id', 'todo_items.task'])->
+            groupBy(['todo_items.id', 'todo_items.task', 'todo_items.completed_at', 'todo_items.user_id'])->
             where('todo_items.user_id', $user_id)->
+            orWhere('shared_items.user_id', $user_id)->
             orderBy(\DB::raw('ISNULL(completed_at)'), 'DESC')->
             orderBy('todo_items.completed_at', 'DESC')->
             orderBy('todo_items.created_at', 'DESC')->
-            get(['todo_items.id', 'todo_items.task', 
+            get(['todo_items.id', 'todo_items.task', 'todo_items.completed_at',
+                \DB::raw('todo_items.user_id = ' . $user_id . ' AS is_mine'),
                 \DB::raw('COUNT(shared_items.todo_item_id) AS share_count')]);
     }
 }
