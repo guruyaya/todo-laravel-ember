6aafe197bcb408697df174933cf15b0b5d5631d6
guruyaya@gmail.com <guruyaya@gmail.com>
2021-06-03 08:27:35 +0300

    Yeah, this one is emberresing.

--- laravel/app/Http/Controllers/ShareController.php
+++ laravel/app/Http/Controllers/ShareController.php
@@ -24,12 +24,27 @@ class ShareController extends Controller
             return ['success'=>false, 'error'=>'task must be created by you, if you want to share it'];
         }
 
-        $share_users = User::leftJoin('shared_items', function($join) use ($item_id) {
+        $share_users = User::where('users.id', '!=', Auth::id())->
+                            leftJoin('shared_items', function($join) use ($item_id) {
                             $join->on('users.id', '=', 'shared_items.user_id')->
                                 where('shared_items.todo_item_id', '=', $item_id);
                         })->orderBy(\DB::raw('ISNULL(shared_items.todo_item_id)'))->
                     get(['Users.name', 'Users.id',
                         \DB::raw('NOT ISNULL(shared_items.todo_item_id) AS is_shared_with')]);
         return ['success'=>true, 'share_users' => $share_users];
+    }
+
+    function share_item(Request $request) {
+        if (!Auth::check()) {
+            return ['success'=>false, 'error' => 'not logged in'];
+        }
+        $item_id = $request->item_id;
+        $task = TodoItem::
+            where('id', $item_id)->
+            where('user_id', Auth::id())->
+            get();
+        if (count($task) == 0) {
+            return ['success'=>false, 'error'=>'task must be created by you, if you want to share it'];
+        }
     }
 }
