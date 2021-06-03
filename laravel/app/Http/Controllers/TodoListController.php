5e441fcebbaae2f18a174e7072f01b2b7b9599da
guruyaya@gmail.com <guruyaya@gmail.com>
2021-06-03 20:17:37 +0300

    Can complete shared missions

--- laravel/app/Http/Controllers/TodoListController.php
+++ laravel/app/Http/Controllers/TodoListController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use App\TodoItem;
+use App\SharedItem;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -66,22 +67,33 @@ class TodoListController extends Controller
         }
 
         $data = $request->post();
-        $task_query = TodoItem::where('id', $data['task_id'])->
+        $task_query = TodoItem::
+            where('id', $data['task_id'])->
             where('user_id', Auth::id());
         $task = $task_query->get();
 
         if (count($task) == 0) {
-            $todo_items = TodoItem::get_user_tasks(Auth::id());
-            return ['success'=>false, 'error'=>'this is not your task', 'todo_items'=>$todo_items];
+            $task_query = SharedItem::
+                where('todo_item_id', $data['task_id'])->
+                where('user_id', Auth::id());
+            $task = $task_query->get();
+            if (count($task) == 0) {
+                $todo_items = TodoItem::get_user_tasks(Auth::id());
+                return ['success'=>false, 'error'=>'this is not your task', 'todo_items'=>$todo_items];
+            }
         }
 
         if ($data['is_done'] == 'true'){
-            $affected = $task_query->update([
+            $affected = TodoItem::
+            where('id', $data['task_id'])->
+            update([
                 'completed_at' => \DB::raw('NOW()'), 
                 'completed_by' => Auth::id()
             ]);
         }else{
-            $affected = $task_query->update([
+            $affected = TodoItem::
+            where('id', $data['task_id'])->
+            update([
                 'completed_at' => null,
                 'completed_by' => null
             ]);
